408b15ebec666f2e35451f52586c3f80452e8c9c
Oleg Gushchin <dikiy3009@gmail.com>
2022-06-21 23:42:53 +0500

    first program C#

---
+++ project001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "ника")
+{
+    Console.WriteLine("Ты самая самая!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
